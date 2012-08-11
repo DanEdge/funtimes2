e55bb729c296403ec812472d7e2f13d3c6d0313d
Dan Edge <dan.edge1@gmail.com>
2012-08-11 21:08:27 +0100

    Remove env array

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 $currentHost = $_SERVER['HTTP_HOST'];
-echo '<pre>';
-print_r($_SERVER);
-echo '</pre>';
+
 echo '<h1>Home page</h1>';
 echo '<a href="git1.php">git1</a>';
 echo '<a href="git2.php">git2</a>';
