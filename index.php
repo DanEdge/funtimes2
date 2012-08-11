822fb88f307da6c5a46b45aa5eb7e9c334caf669
Dan Edge <dan.edge1@gmail.com>
2012-08-11 20:38:03 +0100

    hopefully print out server array

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 $currentHost = $_SERVER['HTTP_HOST'];
-echo $_SERVER['HTTP_HOST'];
+echo '<pre>';
+print_r($_SERVER['HTTP_HOST']);
+echo '</pre>';
 echo '<h1>Home page</h1>';
 echo '<a href="git1.php">git1</a>';
 echo '<a href="git2.php">git2</a>';
