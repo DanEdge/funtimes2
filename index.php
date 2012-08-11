af5492aad929ba5c0e5b52c52bff7ca02d7dd22c
Dan Edge <dan.edge1@gmail.com>
2012-08-11 20:24:11 +0100

    Added apostrophe

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ echo '<h1>Home page</h1>';
 echo '<a href="git1.php">git1</a>';
 echo '<a href="git2.php">git2</a>';
 echo '<br />Made a change';
-echo '<br /> . $currentHost;
-git remote add origin https://github.com/username/Hello-World.git
+echo '<br />' . $currentHost;
 
 ?>
