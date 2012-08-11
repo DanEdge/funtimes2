9aab54eea7c43c93be94affc864a06ed6fc03dbf
Dan Edge <dan.edge1@gmail.com>
2012-08-11 19:25:42 +0100

    Added server env line

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
+$currenthost = $_SERVER['HTTP_HOST'];
 echo '<h1>Home page</h1>';
 echo '<a href="git1.php">git1</a>';
 echo '<a href="git2.php">git2</a>';
 echo '<br />Made a change';
+echo '<br /> . $currentHost;
 ?>
