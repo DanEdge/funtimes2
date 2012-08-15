1ca29279db6d3add6c22f9e9bbf483aa62e84b68
Dan Edge <dan.edge1@gmail.com>
2012-08-15 21:21:32 +0100

    Checking HTTP_HOST

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ echo '<h1>Home page</h1>';
 echo '<a href="git1.php">git1</a>';
 echo '<a href="git2.php">git2</a>';
 echo '<br />Made a change';
-echo '<br />' . $currentHost;
-
+echo '<br />' . 'Current host: ' . $currentHost;
+echo '<br />' . gethostname();
+echo '<pre>';
+print_r($_SERVER);
+echo '</pre>';
 ?>
