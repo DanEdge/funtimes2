d660e9baf71972e2c6d078eca6166c33990cf97a
Dan Edge <dan.edge1@gmail.com>
2012-08-15 21:36:26 +0100

    Checking HTTP_HOST again

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 if ($_SERVER['HTTP_HOST'] == '127.0.0.1' || 'localhost')
 {
-	echo 'HTTP_HOST ' . $_SERVER['HTTP_HOST'];
+	echo $_SERVER['HTTP_HOST'];
 }
 elseif ($_SERVER['HTTP_HOST'] == 'thepeakdistrict.org.uk')
 {
-	echo 'HTTP_HOST ' . $_SERVER['HTTP_HOST'];
+	echo $_SERVER['HTTP_HOST'];
 }
 elseif ($_SERVER['HTTP_HOST'] == 'dev.thepeakdistrict.org.uk')
 {
-	echo 'HTTP_HOST ' . $_SERVER['HTTP_HOST'];
+	echo $_SERVER['HTTP_HOST'];
 }
 ?>
